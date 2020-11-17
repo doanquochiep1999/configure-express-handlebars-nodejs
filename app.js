e1ac5eff8bbc2580a84f28028373f45c8bce5a6c
Quoc Hiep 322 <quochiep322@gmail.com>
2020-11-17 20:03:59 +0700

    send data to express-handlebars

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const app = express();
 const exphbs = require('express-handlebars');
 
 //Set our engine to use our handlebars engine and set view engine to handlebars
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
 // Routing
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Home Page',
+        name: 'Hoai Thien',
+        age: 5
+    });
 })
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {
+        title: 'About Me',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, animi? Facere repellat alias exercitationem quibusdam expedita voluptatibus non. Sed, explicabo cumque facere ratione libero iste nostrum officia eius iusto iure numquam culpa quaerat dolores atque mollitia? Harum repudiandae obcaecati ullam.'
+    });
 })
 
 
